29d9652d3ce28946c43c4fb85ff710ca302d695b
naveenpardeep <naveensheoran1000@gmail.com>
2023-01-24 16:22:02 +0300

    fix

--- lib/forms/user_account/user_notification_controller.dart
+++ lib/forms/user_account/user_notification_controller.dart
@@ -2,27 +2,27 @@ import 'package:get/get.dart';
 import 'package:nsg_data/nsg_data.dart';
 import 'package:task_manager_app/model/data_controller_model.dart';
 
-
-class UserNotificationController extends NsgDataController<UserNotificationSettings> {
+class UserNotificationController
+    extends NsgDataController<UserNotificationSettings> {
   UserNotificationController()
       : super(requestOnInit: false, autoRepeate: true, autoRepeateCount: 100);
-
-  
 
   @override
   Future<NsgDataItem> doCreateNewItem() async {
     var element = await super.doCreateNewItem();
-
-    // element.id = Guid.newGuid();
 
     element.id = Guid.newGuid();
 
     return element;
   }
 
- 
-}
+  @override
+  Future<UserNotificationSettings> createNewItemAsync() async {
+    var element = await super.createNewItemAsync();
 
+    return element;
+  }
+}
 
 class UserNotificationSettingStatusTableController
     extends NsgDataTableController<UserNotificationSettingsStatusTable> {
@@ -31,4 +31,4 @@ class UserNotificationSettingStatusTableController
           masterController: Get.find<UserNotificationController>(),
           tableFieldName: UserNotificationSettingsGenerated.nameStatusTable,
         );
-        }
+}
