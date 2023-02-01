9dc14dcda8e5ffb7ab6f92ed08fbab3b0ea37740
naveenpardeep <naveensheoran1000@gmail.com>
2023-02-01 13:19:47 +0300

    invitation added

--- lib/forms/invitation/accept_invitation.dart
+++ lib/forms/invitation/accept_invitation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:nsg_controls/nsg_controls.dart';
 import 'package:task_manager_app/app_pages.dart';
 
@@ -7,63 +8,95 @@ import 'package:task_manager_app/forms/invitation/invitation_controller.dart';
 import 'package:task_manager_app/forms/project/project_controller.dart';
 import 'package:task_manager_app/model/data_controller.dart';
 
-
-class AcceptInvitationPage extends StatefulWidget {
+class AcceptInvitationPage extends GetView<InvitationController> {
   const AcceptInvitationPage({Key? key}) : super(key: key);
-  @override
-  State<AcceptInvitationPage> createState() => _AcceptInvitationPageState();
-}
-
-class _AcceptInvitationPageState extends State<AcceptInvitationPage> {
-  var controller = Get.find<InvitationController>();
-
-  @override
-  void initState() {
-    super.initState();
-    // if (controller.lateInit) {
-    //   controller.requestItems();
-    // }
-  }
 
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+    if (controller.lateInit) {
+      controller.requestItems();
+    }
+    double height = MediaQuery.of(context).size.height;
     return BodyWrap(
-        child: Scaffold(
-            key: scaffoldKey,
-            backgroundColor: Colors.white,
-            body: controller.obx((state) => Container(
-                key: GlobalKey(),
-                decoration: const BoxDecoration(color: Colors.white),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      NsgAppBar(
-                        color: Colors.white,
-                        backColor: const Color(0xff7876D9),
-                        text: controller.currentItem.isEmpty
-                            ? 'Invitation'.toUpperCase()
-                            : controller.currentItem.invitedUser.name,
-                        icon: Icons.arrow_back_ios_new,
-                        colorsInverted: true,
-                        bottomCircular: true,
-                        onPressed: () {
-                          controller.itemPageCancel();
-                        },
-                        // icon2: Icons.check,
-                        // onPressed2: () async {
-                        //   await controller.itemPagePost();
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: controller.obx(
+          (state) => Container(
+            decoration: const BoxDecoration(color: Colors.white),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                NsgAppBar(
+                  backColor: ControlOptions.instance.colorWhite,
+                  text: 'Invitations List',
+                  icon: Icons.arrow_back_ios_new,
+                  colorsInverted: true,
+                  bottomCircular: true,
+                  onPressed: () {
+                    controller.itemPageCancel();
+                  },
+                  // icon2: Icons.check,
+                  // onPressed2: () {
+                  //   controller.itemPagePost();
+                  // },
+                ),
+                Expanded(
+                  child: Container(
+                      padding: const EdgeInsets.fromLTRB(5, 10, 5, 15),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            commentList(),
+                          ],
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-                        //   Get.find<ProjectController>().newItemPageOpen(
-                        //       pageName: Routes.projectListPage);
-                        // },
-                      ),
-                      Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.fromLTRB(5, 10, 5, 15),
-                          child: SingleChildScrollView(
-                            child: Column(children: [
-                              NsgButton(
+  Widget commentList() {
+    DateFormat formateddate = DateFormat("dd-MM-yyyy   HH:mm:ss");
+    List<Widget> list = [];
+
+    var comments = controller.items;
+
+    for (var comment in comments) {
+      {
+        list.add(GestureDetector(
+          child: Container(
+            color: ControlOptions.instance.colorGreyLighter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: Text(
+                           'Author Name:  ${comment.author}',
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Center(child: Text('Organization Name:  ${comment.organization}')),
+                        Center(
+                          child: Text(
+                            'создано: ${formateddate.format(comment.date)}',
+                            maxLines: 1,
+                            textScaleFactor: 0.8,
+                            style: const TextStyle(color: Color(0xff10051C)),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: NsgButton(
                                 text: 'Accept Invitation',
                                 onPressed: () async {
                                   var dataController =
@@ -79,7 +112,9 @@ class _AcceptInvitationPageState extends State<AcceptInvitationPage> {
                                       pageName: Routes.projectListPage);
                                 },
                               ),
-                               NsgButton(
+                            ),
+                            Expanded(
+                              child: NsgButton(
                                 text: 'Decline Invitation',
                                 onPressed: () async {
                                   var dataController =
@@ -95,20 +130,28 @@ class _AcceptInvitationPageState extends State<AcceptInvitationPage> {
                                       pageName: Routes.projectListPage);
                                 },
                               ),
-                              // NsgInput(
-                              //   dataItem: controller.currentItem,
-                              //   fieldName: InvitationGenerated.nameIsAccepted,
-                              //   label: 'Accept',
-                              // ),
-                              // NsgInput(
-                              //   dataItem: controller.currentItem,
-                              //   fieldName: InvitationGenerated.nameIsRejected,
-                              //   label: 'Reject',
-                              // ),
-                            ]),
-                          ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ])))));
+
+                        Divider()
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+      }
+    }
+
+    return SingleChildScrollView(
+        child: Padding(
+      padding: const EdgeInsets.only(right: 10),
+      child: Column(
+        children: list,
+      ),
+    ));
   }
 }
