5437cd4e211e049290e2ddcf0b2d35536a05ba38
pro5 <pro5@SRV-FRZ-WEB>
2023-02-02 16:21:16 +0300

    userAccount.toString()

--- lib/model/user_account.dart
+++ lib/model/user_account.dart
@@ -1,4 +1,11 @@
 import 'generated/user_account.g.dart';
 
 class UserAccount extends UserAccountGenerated {
+  @override
+  String toString() {
+    if (organization.isNotEmpty) {
+      return '${super.toString()} - ${organization.name}';
+    }
+    return super.toString();
+  }
 }
