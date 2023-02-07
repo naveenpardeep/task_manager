f4d3ee00fd410f9b0fdbb8430cf14d10bc15d806
naveenpardeep <naveensheoran1000@gmail.com>
2023-02-07 16:14:06 +0300

    fixed

--- lib/forms/notification/notification_page.dart
+++ lib/forms/notification/notification_page.dart
@@ -26,45 +26,49 @@ class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-    return BodyWrap(
-        child: Scaffold(
-            key: scaffoldKey,
-            backgroundColor: Colors.white,
-            body: controller.obx((state) => Container(
-                key: GlobalKey(),
-                decoration: const BoxDecoration(color: Colors.white),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      NsgAppBar(
-                        color: Colors.white,
-                        backColor: const Color(0xff7876D9),
-                        text: 'Уведомления'.toUpperCase(),
-                        icon: Icons.arrow_back_ios_new,
-                        colorsInverted: true,
-                        bottomCircular: true,
-                        onPressed: () {
-                          controller.itemPageCancel();
-                        },
-                      ),
-                   
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(5, 10, 5, 15),
-                        child: getNotificationTaskList(),
-                      ),
-                    ])))));
+    double height = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: BodyWrap(
+          child: Scaffold(
+              key: scaffoldKey,
+              backgroundColor: Colors.white,
+              body: controller.obx((state) => Container(
+                  key: GlobalKey(),
+                  decoration: const BoxDecoration(color: Colors.white),
+                  child: SingleChildScrollView(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          NsgAppBar(
+                            color: Colors.white,
+                            backColor: const Color(0xff7876D9),
+                            text: 'Уведомления'.toUpperCase(),
+                            icon: Icons.arrow_back_ios_new,
+                            colorsInverted: true,
+                            bottomCircular: true,
+                            onPressed: () {
+                              controller.itemPageCancel();
+                            },
+                          ),
+                          Container(
+                            height: height,
+                            padding: const EdgeInsets.fromLTRB(5, 10, 5, 15),
+                            child: getNotificationTaskList(),
+                          ),
+                        ]),
+                  ))))),
+    );
   }
 
   Widget getNotificationTaskList() {
     List<Widget> list = [];
-
+    var scrollController = ScrollController();
     var tasksList = controller.items;
 
     for (var tasks in tasksList) {
       {
         list.add(GestureDetector(
           onTap: () {
-           
             // Get.toNamed(Routes.tasksPage);
 
             controller.itemPageOpen(tasks, Routes.tasksPage,
@@ -124,12 +128,21 @@ class _NotificationPageState extends State<NotificationPage> {
       }
     }
 
-    return SingleChildScrollView(
-        child: Padding(
-      padding: const EdgeInsets.only(right: 10),
-      child: Column(
-        children: list,
-      ),
-    ));
+    return RawScrollbar(
+        thumbVisibility: true,
+        trackVisibility: true,
+        controller: scrollController,
+        thickness: 10,
+        trackBorderColor: ControlOptions.instance.colorGreyLight,
+        trackColor: ControlOptions.instance.colorGreyLight,
+        thumbColor: ControlOptions.instance.colorMain.withOpacity(0.2),
+        radius: const Radius.circular(0),
+        child: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: Column(
+            children: list,
+          ),
+        )));
   }
 }
