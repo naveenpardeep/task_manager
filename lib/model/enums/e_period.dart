bba32dcfe6fd317de8a2565767aa0612388becfb
pro5 <pro5@SRV-FRZ-WEB>
2023-02-10 12:38:29 +0300

    EPeriod

--- lib/model/enums/e_period.dart
+++ lib/model/enums/e_period.dart
@@ -2,22 +2,22 @@ import 'package:nsg_data/nsg_data.dart';
 
 /// Период
 class EPeriod extends NsgEnum {
-  static EPeriod day = EPeriod(0, 'День');
-  static EPeriod week = EPeriod(1, 'Неделя');
-  static EPeriod month = EPeriod(2, 'Месяц');
-  static EPeriod year = EPeriod(3, 'Год');
-  static EPeriod all = EPeriod(4, 'Все');
+  static EPeriod all = EPeriod(0, 'Все');
+  static EPeriod day = EPeriod(1, 'День');
+  static EPeriod week = EPeriod(2, 'Неделя');
+  static EPeriod month = EPeriod(3, 'Месяц');
+  static EPeriod year = EPeriod(4, 'Год');
 
   EPeriod(dynamic value, String name) : super(value: value, name: name);
 
   @override
   void initialize() {
     NsgEnum.listAllValues[runtimeType] = <int, EPeriod>{
-      0: day,
-      1: week,
-      2: month,
-      3: year,
-      4: all,
+      0: all,
+      1: day,
+      2: week,
+      3: month,
+      4: year,
     };
   }
 }
