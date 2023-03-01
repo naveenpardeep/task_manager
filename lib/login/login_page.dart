9b42f132e8953622ad9c3d05ef73c7993c5370d7
AndyProtech <andy.protech@gmail.com>
2023-03-01 17:30:08 +0300

    logo

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:nsg_controls/nsg_button.dart';
+import 'package:nsg_controls/nsg_control_options.dart';
 import 'package:nsg_data/nsg_data.dart';
 
 import 'login_params.dart';
@@ -10,29 +10,17 @@ class LoginPage extends NsgPhoneLoginPage {
 
   @override
   Widget getLogo() {
-    const List<String> images = <String>[
-      'lib/assets/images/sv1.svg',
-      'lib/assets/images/sv2.svg',
-      'lib/assets/images/sv3.svg',
-      'lib/assets/images/sv4.svg',
-      'lib/assets/images/logo.svg'
-    ];
-    var logo = Center(
-        child: Transform.translate(
-            offset: const Offset(0, -50),
-            child: Transform.scale(
-              scale: 2,
-              child: Stack(
-                children: [
-                  SvgPicture.asset(images[0]),
-                  Transform.translate(offset: const Offset(13.5, 26), child: SvgPicture.asset(images[1])),
-                  Transform.translate(offset: const Offset(10, 10), child: SvgPicture.asset(images[2])),
-                  Transform.translate(offset: const Offset(37, 0), child: SvgPicture.asset(images[3])),
-                  Transform.translate(offset: const Offset(0, 80), child: SvgPicture.asset(images[4]))
-                ],
-              ),
-            )));
-
+    var logo = Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        width: double.infinity,
+        height: 160,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          ControlOptions.instance.colorMain.withOpacity(0),
+          ControlOptions.instance.colorMain,
+          ControlOptions.instance.colorMain.withOpacity(0),
+        ])),
+        child: Image.asset('lib/assets/logo.png'));
     return logo;
   }
 
