37173f582b92220f6b87521de17d6b74d4fbb041
naveenpardeep <naveensheoran1000@gmail.com>
2023-03-10 18:02:35 +0300

    homepage fixed for refresh and margin

--- lib/forms/widgets/task_tuner_button.dart
+++ lib/forms/widgets/task_tuner_button.dart
@@ -54,7 +54,7 @@ class TaskIconButton extends StatelessWidget {
     return InkWell(
         onTap: onTap,
         child: Container(
-            margin: const EdgeInsets.all(10),
+            margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
             padding: const EdgeInsets.all(10),
             decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: getColor(style)),
             child: Stack(children: [
