104095bd421fd297f93ba072b094b75c122052b1
naveenpardeep <naveensheoran1000@gmail.com>
2023-03-13 14:54:01 +0300

    new mobile menu

--- lib/forms/widgets/mobile_menu.dart
+++ lib/forms/widgets/mobile_menu.dart
@@ -23,148 +23,170 @@ class TmMobileMenu extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-              child: InkWell(
-            onTap: () {
-              Get.find<ProjectController>().refreshData();
-              Get.toNamed(Routes.projectListPage);
-            },
-            child: Icon(
-              Icons.hub,
-              color: ControlOptions.instance.colorMain,
-            ),
-          )),
-          Expanded(
-              child: InkWell(
-            onTap: () {
-              Get.find<TasksController>().refreshData();
-              Get.toNamed(Routes.tasksListPage);
-            },
-            child: Icon(
-              Icons.task,
-              color: ControlOptions.instance.colorMain,
-            ),
-          )),
-          Expanded(
-              child: InkWell(
-            onTap: () {
-              Get.toNamed(Routes.userAccountListPage);
-            },
-            child: Icon(
-              Icons.groups,
-              color: ControlOptions.instance.colorMain,
-            ),
-          )),
-          Expanded(
             child: InkWell(
                 onTap: () {
-                  Get.toNamed(Routes.organizationListMobilePage);
-
-                  // Get.toNamed(Routes.organizationPage);
-                  //  Get.find<OrganizationController>().newItemPageOpen(pageName: Routes.organizationPage );
+                  Get.find<ProjectController>().refreshData();
+                  Get.toNamed(Routes.projectListPage);
                 },
-                child: Icon(
-                  Icons.apartment,
-                  color: ControlOptions.instance.colorMain,
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.folder,
+                      color: ControlOptions.instance.colorMain,
+                    ),
+                    const Text('Проекты')
+                  ],
                 )),
           ),
           Expanded(
-              child: InkWell(
-            onTap: () {
-              Get.find<InvitationController>().refreshData();
-              Get.find<InvitationController>().newItemPageOpen(pageName: Routes.invitationAcceptNew);
-            },
-            child: Icon(
-              Icons.insert_invitation,
-              color: ControlOptions.instance.colorMain,
-            ),
-          )),
-          Expanded(
             child: InkWell(
-              onTap: () {
-                Get.find<AccpetController>().refreshData();
-                Get.find<AccpetController>().newItemPageOpen(pageName: Routes.acceptRejectListPage);
-              },
-              child: Icon(
-                Icons.list_alt,
-                color: ControlOptions.instance.colorMain,
-              ),
-            ),
-          ),
-          Stack(
-            children: <Widget>[
-              InkWell(
                 onTap: () {
-                  Get.toNamed(Routes.notificationPage);
-                  //  Get.find<NotificationController>().newItemPageOpen(pageName: Routes.notificationPage
-                  // );
+                  Get.find<TasksController>().refreshData();
+                  Get.toNamed(Routes.tasksListPage);
                 },
-                child: Icon(
-                  Icons.notifications,
-                  color: ControlOptions.instance.colorMain,
-                ),
-              ),
-              Positioned(
-                right: 1,
-                top: 1,
-                child: Container(
-                  padding: const EdgeInsets.all(2),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(6),
-                  ),
-                  constraints: const BoxConstraints(
-                    minWidth: 15,
-                    minHeight: 15,
-                  ),
-                  child: Text(
-                    Get.find<NotificationController>().items.length.toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 10,
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.task,
+                      color: ControlOptions.instance.colorMain,
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              )
-            ],
+                    const Text('Задачи')
+                  ],
+                )),
           ),
-          Expanded(
-            child: Center(
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  InkWell(
+               Expanded(
+                 child: InkWell(
+                  onTap: () {
+                             Get.toNamed(Routes.userAccountListPage);
+                  },
+                  child:
+                         Column(
+                           children: [
+                             Icon(
+                             Icons.people,
+                             color: ControlOptions.instance.colorMain,
+                  ),
+                           const  Text( 'Сотрудники')
+                           ],)
+                         ),
+               ),
+           Expanded(
+             child: InkWell(
                     onTap: () {
-                      // Get.toNamed(Routes.userProfilePage);
-                      //Get.find<UserAccountController>()
-                      //    .itemPageOpen(Get.find<DataController>().currentUser, Routes.userProfilePage, needRefreshSelectedItem: true);
-                      Get.find<UserAccountController>()
-                          .itemPageOpen(Get.find<DataController>().currentUser, Routes.profileViewPage, needRefreshSelectedItem: true);
+                      Get.toNamed(Routes.organizationListMobilePage);
+           
+                      // Get.toNamed(Routes.organizationPage);
+                      //  Get.find<OrganizationController>().newItemPageOpen(pageName: Routes.organizationPage );
                     },
-                    child: ClipOval(
-                      child: Get.find<DataController>().currentUser.photoFile.isEmpty
-                          ? Container(
-                              decoration: BoxDecoration(color: ControlOptions.instance.colorMain.withOpacity(0.2)),
-                              width: 32,
-                              height: 32,
-                              child: Icon(
-                                Icons.account_circle,
-                                size: 20,
-                                color: ControlOptions.instance.colorMain.withOpacity(0.4),
-                              ),
-                            )
-                          : Image.memory(
-                              Uint8List.fromList(Get.find<DataController>().currentUser.photoFile),
-                              fit: BoxFit.cover,
-                              width: 32,
-                              height: 32,
-                            ),
+                    child: 
+                     Column(
+              children: [
+               Icon(
+                      Icons.apartment,
+                      color: ControlOptions.instance.colorMain,
                     ),
+                    const Text('Компании')
+              ],)
+                     ),
+           ),
+          // Expanded(
+          //     child: InkWell(
+          //   onTap: () {
+          //     Get.find<InvitationController>().refreshData();
+          //     Get.find<InvitationController>().newItemPageOpen(pageName: Routes.invitationAcceptNew);
+          //   },
+          //   child: Icon(
+          //     Icons.insert_invitation,
+          //     color: ControlOptions.instance.colorMain,
+          //   ),
+          // )),
+          // Expanded(
+          //   child: InkWell(
+          //     onTap: () {
+          //       Get.find<AccpetController>().refreshData();
+          //       Get.find<AccpetController>().newItemPageOpen(pageName: Routes.acceptRejectListPage);
+          //     },
+          //     child: Icon(
+          //       Icons.list_alt,
+          //       color: ControlOptions.instance.colorMain,
+          //     ),
+          //   ),
+          // ),
+          // Stack(
+          //   children: <Widget>[
+          //     InkWell(
+          //       onTap: () {
+          //         Get.toNamed(Routes.notificationPage);
+          //         //  Get.find<NotificationController>().newItemPageOpen(pageName: Routes.notificationPage
+          //         // );
+          //       },
+          //       child: Icon(
+          //         Icons.notifications,
+          //         color: ControlOptions.instance.colorMain,
+          //       ),
+          //     ),
+          //     Positioned(
+          //       right: 1,
+          //       top: 1,
+          //       child: Container(
+          //         padding: const EdgeInsets.all(2),
+          //         decoration: BoxDecoration(
+          //           color: Colors.red,
+          //           borderRadius: BorderRadius.circular(6),
+          //         ),
+          //         constraints: const BoxConstraints(
+          //           minWidth: 15,
+          //           minHeight: 15,
+          //         ),
+          //         child: Text(
+          //           Get.find<NotificationController>().items.length.toString(),
+          //           style: const TextStyle(
+          //             color: Colors.white,
+          //             fontSize: 10,
+          //           ),
+          //           textAlign: TextAlign.center,
+          //         ),
+          //       ),
+          //     )
+          //   ],
+          // ),
+
+           Expanded(
+             child: InkWell(
+                  onTap: () {
+                    // Get.toNamed(Routes.userProfilePage);
+                    //Get.find<UserAccountController>()
+                    //    .itemPageOpen(Get.find<DataController>().currentUser, Routes.userProfilePage, needRefreshSelectedItem: true);
+                    Get.find<UserAccountController>()
+                        .itemPageOpen(Get.find<DataController>().currentUser, Routes.profileViewPage, needRefreshSelectedItem: true);
+                  },
+                  child:
+                     Column(
+              children: [
+                ClipOval(
+                    child: Get.find<DataController>().currentUser.photoFile.isEmpty
+                        ? Container(
+                            decoration: BoxDecoration(color: ControlOptions.instance.colorMain.withOpacity(0.2)),
+                            width: 25,
+                            height: 25,
+                            child: Icon(
+                              Icons.account_circle,
+                              size: 20,
+                              color: ControlOptions.instance.colorMain.withOpacity(0.4),
+                            ),
+                          )
+                        : Image.memory(
+                            Uint8List.fromList(Get.find<DataController>().currentUser.photoFile),
+                            fit: BoxFit.cover,
+                            width: 25,
+                            height: 25,
+                          ),
                   ),
-                ],
-              ),
-            ),
-          ),
+                  const Text('Аккаунт')
+                
+              ],
+                     ),),
+           )
         ],
       ),
     );
