48d2ca569fb82e3e2c8f74830b052b67f18eb4e8
naveenpardeep <naveensheoran1000@gmail.com>
2023-03-13 10:37:07 +0300

    notification page

--- lib/forms/notification/notification_page.dart
+++ lib/forms/notification/notification_page.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 import 'package:nsg_controls/nsg_controls.dart';
 import 'package:task_manager_app/app_pages.dart';
 import 'package:task_manager_app/forms/notification/notification_controller.dart';
-
+import 'package:task_manager_app/model/enums.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({Key? key}) : super(key: key);
@@ -38,34 +38,30 @@ class _NotificationPageState extends State<NotificationPage> {
                   decoration: const BoxDecoration(color: Colors.white),
                   child: SingleChildScrollView(
                     physics: const BouncingScrollPhysics(),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          NsgAppBar(
-                            color: Colors.white,
-                            backColor: const Color(0xff7876D9),
-                            text: 'Уведомления'.toUpperCase(),
-                            icon: Icons.arrow_back_ios_new,
-                            colorsInverted: true,
-                            bottomCircular: true,
-                            onPressed: () {
-                              controller.itemPageCancel();
-                            },
-                          ),
-                          Container(
-                            height: height,
-                            padding: const EdgeInsets.fromLTRB(5, 10, 5, 15),
-                            child: getNotificationTaskList(),
-                          ),
-                        ]),
+                    child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+                      NsgAppBar(
+                        color: Colors.white,
+                        backColor: const Color(0xff7876D9),
+                        text: 'Уведомления'.toUpperCase(),
+                        icon: Icons.arrow_back_ios_new,
+                        colorsInverted: true,
+                        bottomCircular: true,
+                        onPressed: () {
+                          controller.itemPageCancel();
+                        },
+                      ),
+                      getNotificationInvitationList(),
+                      getNotificationNewTaskList(),
+                      getNotificationEditedTaskList(),
+                    ]),
                   ))))),
     );
   }
 
-  Widget getNotificationTaskList() {
+  Widget getNotificationInvitationList() {
     List<Widget> list = [];
     var scrollController = ScrollController();
-    var tasksList = controller.items.reversed;
+    var tasksList = controller.items.reversed.where((element) => element.notificationType == ENotificationType.invitationAccepted);
 
     for (var tasks in tasksList) {
       {
@@ -73,8 +69,7 @@ class _NotificationPageState extends State<NotificationPage> {
           onTap: () {
             // Get.toNamed(Routes.tasksPage);
 
-            controller.itemPageOpen(tasks, Routes.acceptInvitationPage,
-                needRefreshSelectedItem: true);
+            controller.itemPageOpen(tasks, Routes.acceptInvitationPage, needRefreshSelectedItem: true);
           },
           child: Row(
             children: [
@@ -98,8 +93,6 @@ class _NotificationPageState extends State<NotificationPage> {
                                     child: Text(
                                       'Invitation Sent by: ${tasks.invitation.author.name}',
                                       maxLines: 2,
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.bold),
                                     ),
                                   ),
                                 ],
@@ -115,8 +108,172 @@ class _NotificationPageState extends State<NotificationPage> {
                                     'создано: ${formateddate.format(tasks.date)}',
                                     maxLines: 1,
                                     textScaleFactor: 0.8,
-                                    style: const TextStyle(
-                                        color: Color(0xff10051C)),
+                                    style: const TextStyle(color: Color(0xff10051C)),
+                                  )),
+                                ],
+                              )
+                            ],
+                          ),
+                        ))),
+              ),
+            ],
+          ),
+        ));
+      }
+    }
+
+    return RawScrollbar(
+        thumbVisibility: true,
+        trackVisibility: true,
+        controller: scrollController,
+        thickness: 10,
+        trackBorderColor: ControlOptions.instance.colorGreyLight,
+        trackColor: ControlOptions.instance.colorGreyLight,
+        thumbColor: ControlOptions.instance.colorMain.withOpacity(0.2),
+        radius: const Radius.circular(0),
+        child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            controller: scrollController,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Column(
+                children: list,
+              ),
+            )));
+  }
+
+  Widget getNotificationNewTaskList() {
+    List<Widget> list = [];
+    var scrollController = ScrollController();
+    var newtasksList = controller.items.reversed.where((element) => element.notificationType == ENotificationType.newTask);
+
+    for (var tasks in newtasksList) {
+      {
+        list.add(GestureDetector(
+          onTap: () {
+            controller.itemPageOpen(tasks, Routes.tasksPage, needRefreshSelectedItem: true);
+          },
+          child: Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                    height: 98,
+                    child: Card(
+                        color: const Color.fromARGB(239, 248, 250, 252),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                tasks.task.name,
+                                maxLines: 1,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      '${tasks.task}',
+                                      maxLines: 2,
+                                      style: const TextStyle(fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.access_time,
+                                    size: 12,
+                                  ),
+                                  Expanded(
+                                      child: Text(
+                                    'создано: ${formateddate.format(tasks.date)}',
+                                    maxLines: 1,
+                                    textScaleFactor: 0.8,
+                                    style: const TextStyle(color: Color(0xff10051C)),
+                                  )),
+                                ],
+                              )
+                            ],
+                          ),
+                        ))),
+              ),
+            ],
+          ),
+        ));
+      }
+    }
+
+    return RawScrollbar(
+        thumbVisibility: true,
+        trackVisibility: true,
+        controller: scrollController,
+        thickness: 10,
+        trackBorderColor: ControlOptions.instance.colorGreyLight,
+        trackColor: ControlOptions.instance.colorGreyLight,
+        thumbColor: ControlOptions.instance.colorMain.withOpacity(0.2),
+        radius: const Radius.circular(0),
+        child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            controller: scrollController,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Column(
+                children: list,
+              ),
+            )));
+  }
+
+  Widget getNotificationEditedTaskList() {
+    List<Widget> list = [];
+    var scrollController = ScrollController();
+    var newtasksList = controller.items.reversed.where((element) => element.notificationType == ENotificationType.editedTask);
+
+    for (var tasks in newtasksList) {
+      {
+        list.add(GestureDetector(
+          onTap: () {
+            controller.itemPageOpen(tasks, Routes.tasksPage, needRefreshSelectedItem: true);
+          },
+          child: Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                    height: 98,
+                    child: Card(
+                        color: const Color.fromARGB(239, 248, 250, 252),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                tasks.task.name,
+                                maxLines: 1,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      '${tasks.task}',
+                                      maxLines: 2,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.access_time,
+                                    size: 12,
+                                  ),
+                                  Expanded(
+                                      child: Text(
+                                    'создано: ${formateddate.format(tasks.date)}',
+                                    maxLines: 1,
+                                    textScaleFactor: 0.8,
+                                    style: const TextStyle(color: Color(0xff10051C)),
                                   )),
                                 ],
                               )
