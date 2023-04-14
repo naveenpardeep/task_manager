5ac129de4287170b1c062d822482085778a8fe87
Mishasik <mishelzenkov@gmail.com>
2023-04-15 01:51:09 +0300

    fix

--- lib/forms/project/project_list_page.dart
+++ lib/forms/project/project_list_page.dart
@@ -153,151 +153,142 @@ class ProjectItemView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 10, right: 10, bottom: 15),
+    return InkWell(
+      hoverColor: Colors.transparent,
+      onTap: () {
+        var taskConstroller = Get.find<TasksController>();
+        taskConstroller.refreshData();
+        Get.find<TaskBoardController>().refreshData();
+        controller.itemPageOpen(project, Routes.homePage, needRefreshSelectedItem: true);
+      },
+      onLongPress: () {
+        pinDialog(context, project);
+      },
       child: Container(
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(left: 10, right: 10, bottom: 15),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
           color: const Color(0xffEDEFF3),
         ),
-        child: Row(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: InkWell(
-                onTap: () {
-                  var taskConstroller = Get.find<TasksController>();
-                  taskConstroller.refreshData();
-                  Get.find<TaskBoardController>().refreshData();
-                  controller.itemPageOpen(project, Routes.homePage, needRefreshSelectedItem: true);
-                },
-                onLongPress: () {
-                  pinDialog(context, project);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Directionality(
-                                  textDirection: TextDirection.ltr,
-                                  child: SubstringHighlight(
-                                    text: project.name,
-                                    term: searchvalue,
-                                    textStyle: const TextStyle(fontFamily: 'Inter', fontSize: 20, color: Colors.black),
-                                    textStyleHighlight: const TextStyle(color: Colors.deepOrange),
-                                  )),
-                            ),
-                            if (project.isPinned) const Icon(Icons.push_pin, color: Colors.lightBlue),
-                            GestureDetector(
-                              onTapDown: (TapDownDetails details) {
-                                showPopUpMenu(details.globalPosition, project, context);
-                              },
-                              child: IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Expanded(
-                              child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Рук.: ${project.leader.name}', style: const TextStyle(fontFamily: 'Inter', fontSize: 14, color: Color(0xff529FBF))),
-                              Text('Организация: ${project.organization}', style: const TextStyle(fontFamily: 'Inter', fontSize: 14, color: Color(0xff529FBF))),
-                              Text('Заказчик: ${project.contractor}', style: const TextStyle(fontFamily: 'Inter', fontSize: 14, color: Color(0xff529FBF))),
-                            ],
-                          )),
-                          if (project.numberOfNotifications > 0)
-                            Tooltip(
-                              message: 'Number of Notifications',
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 5),
-                                child: NsgCircle(
-                                  text: project.numberOfNotifications.toString(),
-                                  fontSize: 14,
-                                  borderWidth: 1.3,
-                                  color: ControlOptions.instance.colorText,
-                                  borderColor: ControlOptions.instance.colorBlue,
-                                  shadow: const BoxShadow(),
-                                ),
-                              ),
-                            ),
-                          if (project.numberOfTasksUpdatedIn24Hours > 0)
-                            Tooltip(
-                              message: 'Tasks Updated In 24Hours',
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 5),
-                                child: NsgCircle(
-                                  text: project.numberOfTasksUpdatedIn24Hours.toString(),
-                                  fontSize: 14,
-                                  borderWidth: 1.3,
-                                  color: ControlOptions.instance.colorText,
-                                  borderColor: ControlOptions.instance.colorWarning,
-                                  shadow: const BoxShadow(),
-                                ),
-                              ),
-                            ),
-                          if (project.numberOfTasksOverdue > 0)
-                            Tooltip(
-                              message: 'Overdue Tasks',
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 5),
-                                child: NsgCircle(
-                                  text: project.numberOfTasksOverdue.toString(),
-                                  fontSize: 14,
-                                  borderWidth: 1.3,
-                                  color: ControlOptions.instance.colorText,
-                                  borderColor: ControlOptions.instance.colorError,
-                                  shadow: const BoxShadow(),
-                                ),
-                              ),
-                            ),
-                          if (project.numberOfTasksOpen > 0)
-                            Tooltip(
-                              message: 'Tasks open',
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 5),
-                                child: NsgCircle(
-                                  text: project.numberOfTasksOpen.toString(),
-                                  fontSize: 14,
-                                  borderWidth: 1.3,
-                                  color: ControlOptions.instance.colorText,
-                                  shadow: const BoxShadow(),
-                                ),
-                              ),
-                            ),
-                          const Padding(padding: EdgeInsets.only(left: 10)),
-                          ClipOval(
-                              child: project.photoPath.isEmpty
-                                  ? Container(
-                                      decoration: BoxDecoration(color: ControlOptions.instance.colorMain.withOpacity(0.2)),
-                                      width: 32,
-                                      height: 32,
-                                      child: Icon(
-                                        Icons.account_circle,
-                                        size: 20,
-                                        color: ControlOptions.instance.colorMain.withOpacity(0.4),
-                                      ),
-                                    )
-                                  : Image.network(
-                                      TaskFilesController.getFilePath(project.photoPath),
-                                      fit: BoxFit.cover,
-                                      width: 32,
-                                      height: 32,
-                                    )),
-                        ],
-                      ),
-                    ],
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Directionality(
+                        textDirection: TextDirection.ltr,
+                        child: SubstringHighlight(
+                          text: project.name,
+                          term: searchvalue,
+                          textStyle: const TextStyle(fontFamily: 'Inter', fontSize: 20, color: Colors.black),
+                          textStyleHighlight: const TextStyle(color: Colors.deepOrange),
+                        )),
                   ),
-                ),
+                  if (project.isPinned) const Icon(Icons.push_pin, color: Colors.lightBlue),
+                  GestureDetector(
+                    onTapDown: (TapDownDetails details) {
+                      showPopUpMenu(details.globalPosition, project, context);
+                    },
+                    child: IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+                  ),
+                ],
               ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Рук.: ${project.leader.name}', style: const TextStyle(fontFamily: 'Inter', fontSize: 14, color: Color(0xff529FBF))),
+                    Text('Организация: ${project.organization}', style: const TextStyle(fontFamily: 'Inter', fontSize: 14, color: Color(0xff529FBF))),
+                    Text('Заказчик: ${project.contractor}', style: const TextStyle(fontFamily: 'Inter', fontSize: 14, color: Color(0xff529FBF))),
+                  ],
+                )),
+                if (project.numberOfNotifications > 0)
+                  Tooltip(
+                    message: 'Number of Notifications',
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: NsgCircle(
+                        text: project.numberOfNotifications.toString(),
+                        fontSize: 14,
+                        borderWidth: 1.3,
+                        color: ControlOptions.instance.colorText,
+                        borderColor: ControlOptions.instance.colorBlue,
+                        shadow: const BoxShadow(),
+                      ),
+                    ),
+                  ),
+                if (project.numberOfTasksUpdatedIn24Hours > 0)
+                  Tooltip(
+                    message: 'Tasks Updated In 24Hours',
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: NsgCircle(
+                        text: project.numberOfTasksUpdatedIn24Hours.toString(),
+                        fontSize: 14,
+                        borderWidth: 1.3,
+                        color: ControlOptions.instance.colorText,
+                        borderColor: ControlOptions.instance.colorWarning,
+                        shadow: const BoxShadow(),
+                      ),
+                    ),
+                  ),
+                if (project.numberOfTasksOverdue > 0)
+                  Tooltip(
+                    message: 'Overdue Tasks',
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: NsgCircle(
+                        text: project.numberOfTasksOverdue.toString(),
+                        fontSize: 14,
+                        borderWidth: 1.3,
+                        color: ControlOptions.instance.colorText,
+                        borderColor: ControlOptions.instance.colorError,
+                        shadow: const BoxShadow(),
+                      ),
+                    ),
+                  ),
+                if (project.numberOfTasksOpen > 0)
+                  Tooltip(
+                    message: 'Tasks open',
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: NsgCircle(
+                        text: project.numberOfTasksOpen.toString(),
+                        fontSize: 14,
+                        borderWidth: 1.3,
+                        color: ControlOptions.instance.colorText,
+                        shadow: const BoxShadow(),
+                      ),
+                    ),
+                  ),
+                const Padding(padding: EdgeInsets.only(left: 10)),
+                ClipOval(
+                    child: project.photoPath.isEmpty
+                        ? Container(
+                            decoration: BoxDecoration(color: ControlOptions.instance.colorMain.withOpacity(0.2)),
+                            width: 32,
+                            height: 32,
+                            child: Icon(
+                              Icons.account_circle,
+                              size: 20,
+                              color: ControlOptions.instance.colorMain.withOpacity(0.4),
+                            ),
+                          )
+                        : Image.network(
+                            TaskFilesController.getFilePath(project.photoPath),
+                            fit: BoxFit.cover,
+                            width: 32,
+                            height: 32,
+                          )),
+              ],
             ),
           ],
         ),
