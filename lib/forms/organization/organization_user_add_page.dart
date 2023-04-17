59e1778ec206d087391c72dc0b27878d538d0bc2
naveenpardeep <naveensheoran1000@gmail.com>
2023-04-17 16:30:01 +0300

    fix org users card view

--- lib/forms/organization/organization_user_add_page.dart
+++ lib/forms/organization/organization_user_add_page.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:nsg_controls/nsg_controls.dart';
 import 'package:task_manager_app/app_pages.dart';
 import 'package:task_manager_app/forms/organization/organization_controller.dart';
+import 'package:task_manager_app/forms/tasks/task_file_controller.dart';
 import 'package:task_manager_app/forms/user_account/user_account_controller.dart';
 
 class OrganizationUserAddPage extends StatefulWidget {
@@ -128,62 +129,56 @@ class _OrganizationUserAddPageState extends State<OrganizationUserAddPage> {
           orguser.userAccount.phoneNumber.toString().toLowerCase().contains(searchvalue.toLowerCase())) {
         list.add(Padding(
           padding: const EdgeInsets.only(left: 10, right: 10, bottom: 15),
-          child: Card(
-            margin: EdgeInsets.zero,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipOval(
-                      child: orguser.userAccount.photoFile.isEmpty
-                          ? Container(
-                              decoration: BoxDecoration(color: ControlOptions.instance.colorMain.withOpacity(0.2)),
-                              width: 32,
-                              height: 32,
-                              child: Icon(
-                                Icons.account_circle,
-                                size: 20,
-                                color: ControlOptions.instance.colorMain.withOpacity(0.4),
-                              ),
-                            )
-                          : Image.memory(
-                              Uint8List.fromList(orguser.userAccount.photoFile),
-                              fit: BoxFit.cover,
-                              width: 32,
-                              height: 32,
-                            ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          orguser.userAccount.name,
-                          style: TextStyle(fontSize: ControlOptions.instance.sizeL, fontWeight: FontWeight.bold),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: ClipOval(
+                  child: orguser.userAccount.photoName.isEmpty
+                      ? Container(
+                          decoration: BoxDecoration(color: ControlOptions.instance.colorMain.withOpacity(0.2)),
+                          width: 48,
+                          height: 48,
+                          child: Icon(
+                            Icons.account_circle,
+                            size: 20,
+                            color: ControlOptions.instance.colorMain.withOpacity(0.4),
+                          ),
+                        )
+                      : Image.network(
+                          TaskFilesController.getFilePath(orguser.userAccount.photoName),
+                          fit: BoxFit.cover,
+                          width: 48,
+                          height: 48,
                         ),
-                        Text(
-                          orguser.userAccount.phoneNumber,
-                          style: TextStyle(fontSize: ControlOptions.instance.sizeM, color: const Color(0xff529FBF)),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                      width: 30,
-                      child: NsgCheckBox(
-                          toggleInside: true,
-                          key: GlobalKey(),
-                          label: '',
-                          value: orguser.isChecked,
-                          onPressed: (currentValue) {
-                            orguser.isChecked = currentValue;
-                          })),
-                ],
+                ),
               ),
-            ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      orguser.userAccount.name,
+                      style: TextStyle(fontSize: ControlOptions.instance.sizeL, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      orguser.userAccount.phoneNumber,
+                      style: TextStyle(fontSize: ControlOptions.instance.sizeM, color: const Color(0xff529FBF)),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                  width: 30,
+                  child: NsgCheckBox(
+                      toggleInside: true,
+                      key: GlobalKey(),
+                      label: '',
+                      value: orguser.isChecked,
+                      onPressed: (currentValue) {
+                        orguser.isChecked = currentValue;
+                      })),
+            ],
           ),
         ));
       }
@@ -227,7 +222,7 @@ class _OrganizationUserAddPageState extends State<OrganizationUserAddPage> {
                     Padding(
                       padding: const EdgeInsets.all(4.0),
                       child: ClipOval(
-                        child: orguser.userAccount.photoFile.isEmpty
+                        child: orguser.userAccount.photoName.isEmpty
                             ? Container(
                                 decoration: BoxDecoration(color: ControlOptions.instance.colorMain.withOpacity(0.2)),
                                 width: 48,
@@ -238,8 +233,8 @@ class _OrganizationUserAddPageState extends State<OrganizationUserAddPage> {
                                   color: ControlOptions.instance.colorMain.withOpacity(0.4),
                                 ),
                               )
-                            : Image.memory(
-                                Uint8List.fromList(orguser.userAccount.photoFile),
+                            : Image.network(
+                                TaskFilesController.getFilePath(orguser.userAccount.photoName),
                                 fit: BoxFit.cover,
                                 width: 48,
                                 height: 48,
