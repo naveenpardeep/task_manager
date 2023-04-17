f5e2891dd69eaa673d4aa4200898c25fad22a52e
Mishasik <mishelzenkov@gmail.com>
2023-04-17 12:45:20 +0300

    filters

--- lib/view/task_load_controller.dart
+++ lib/view/task_load_controller.dart
@@ -1,8 +1,13 @@
 import 'package:get/get.dart';
 import 'package:nsg_data/nsg_data.dart';
-import 'package:task_manager_app/forms/tasks/tasks_controller.dart';
 
+import '../forms/project/project_controller.dart';
+import '../forms/task_board/task_board_controller.dart';
+import '../forms/user_account/service_object_controller.dart';
+import '../model/enums/e_period.dart';
+import '../model/enums/e_sorting.dart';
 import '../model/generated/task_doc.g.dart';
+import '../model/generated/user_account.g.dart';
 import '../model/task_doc.dart';
 import '../model/task_status.dart';
 
@@ -22,6 +27,111 @@ class TaskLoadController extends NsgBaseController {
   Future<List<TaskDoc>> _getTasksFromStatus(TaskStatus status, int top, int count) async {
     var tasks = NsgDataRequest<TaskDoc>(dataItemType: TaskDoc);
     var filter = NsgDataRequestParams(top: top, count: count);
+
+    var projectController = Get.find<ProjectController>();
+    var taskBoardController = Get.find<TaskBoardController>();
+    var finalTasks = NsgCompare();
+    var notfinalTask = NsgCompare();
+    List<String> notfinalStatusId = [];
+    List<String> statusId = [];
+    taskBoardController.currentItem.statusTable.rows.where((element) => element.status.isDone).toList().forEach((element) {
+      statusId.add(element.statusId);
+    });
+
+    taskBoardController.currentItem.statusTable.rows.where((element) => !element.status.isDone).toList().forEach((element) {
+      notfinalStatusId.add(element.statusId);
+    });
+    if (taskBoardController.currentItem.periodOfFinishedTasks == EPeriod.day) {
+      notfinalTask.add(name: TaskDocGenerated.nameTaskStatusId, value: notfinalStatusId, comparisonOperator: NsgComparisonOperator.inList);
+      finalTasks.add(
+          name: TaskDocGenerated.nameDateUpdated,
+          value: DateTime.now().add(const Duration(days: -1)),
+          comparisonOperator: NsgComparisonOperator.greaterOrEqual);
+      finalTasks.add(name: TaskDocGenerated.nameTaskStatusId, value: statusId, comparisonOperator: NsgComparisonOperator.inList);
+
+      var taskCondtion = NsgCompare();
+      taskCondtion.logicalOperator = NsgLogicalOperator.or;
+      taskCondtion.add(name: 'one', value: notfinalTask);
+      taskCondtion.add(name: 'two', value: finalTasks);
+      filter.compare.add(name: 'three', value: taskCondtion);
+    }
+    if (taskBoardController.currentItem.periodOfFinishedTasks == EPeriod.week) {
+      notfinalTask.add(name: TaskDocGenerated.nameTaskStatusId, value: notfinalStatusId, comparisonOperator: NsgComparisonOperator.inList);
+      finalTasks.add(
+          name: TaskDocGenerated.nameDateUpdated,
+          value: DateTime.now().add(const Duration(days: -7)),
+          comparisonOperator: NsgComparisonOperator.greaterOrEqual);
+      finalTasks.add(name: TaskDocGenerated.nameTaskStatusId, value: statusId, comparisonOperator: NsgComparisonOperator.inList);
+
+      var taskCondtion = NsgCompare();
+      taskCondtion.logicalOperator = NsgLogicalOperator.or;
+      taskCondtion.add(name: 'one', value: notfinalTask);
+      taskCondtion.add(name: 'two', value: finalTasks);
+      filter.compare.add(name: 'three', value: taskCondtion);
+    }
+    if (taskBoardController.currentItem.periodOfFinishedTasks == EPeriod.month) {
+      notfinalTask.add(name: TaskDocGenerated.nameTaskStatusId, value: notfinalStatusId, comparisonOperator: NsgComparisonOperator.inList);
+      finalTasks.add(
+          name: TaskDocGenerated.nameDateUpdated,
+          value: DateTime.now().add(const Duration(days: -31)),
+          comparisonOperator: NsgComparisonOperator.greaterOrEqual);
+      finalTasks.add(name: TaskDocGenerated.nameTaskStatusId, value: statusId, comparisonOperator: NsgComparisonOperator.inList);
+
+      var taskCondtion = NsgCompare();
+      taskCondtion.logicalOperator = NsgLogicalOperator.or;
+      taskCondtion.add(name: 'one', value: notfinalTask);
+      taskCondtion.add(name: 'two', value: finalTasks);
+      filter.compare.add(name: 'three', value: taskCondtion);
+    }
+    if (taskBoardController.currentItem.periodOfFinishedTasks == EPeriod.year) {
+      notfinalTask.add(name: TaskDocGenerated.nameTaskStatusId, value: notfinalStatusId, comparisonOperator: NsgComparisonOperator.inList);
+      finalTasks.add(
+          name: TaskDocGenerated.nameDateUpdated,
+          value: DateTime.now().add(const Duration(days: -365)),
+          comparisonOperator: NsgComparisonOperator.greaterOrEqual);
+      finalTasks.add(name: TaskDocGenerated.nameTaskStatusId, value: statusId, comparisonOperator: NsgComparisonOperator.inList);
+      var taskCondtion = NsgCompare();
+      taskCondtion.logicalOperator = NsgLogicalOperator.or;
+      taskCondtion.add(name: 'one', value: notfinalTask);
+      taskCondtion.add(name: 'two', value: finalTasks);
+      filter.compare.add(name: 'three', value: taskCondtion);
+    }
+    if (taskBoardController.currentItem.periodOfFinishedTasks == EPeriod.all) {
+      notfinalTask.add(name: TaskDocGenerated.nameTaskStatusId, value: notfinalStatusId, comparisonOperator: NsgComparisonOperator.inList);
+      finalTasks.add(name: TaskDocGenerated.nameTaskStatusId, value: statusId, comparisonOperator: NsgComparisonOperator.inList);
+
+      var taskCondtion = NsgCompare();
+      taskCondtion.logicalOperator = NsgLogicalOperator.or;
+      taskCondtion.add(name: 'one', value: notfinalTask);
+      taskCondtion.add(name: 'two', value: finalTasks);
+      filter.compare.add(name: 'three', value: taskCondtion);
+    }
+    var serviceC = Get.find<ServiceObjectController>();
+
+    // Если указан ID пользователя, то фильтруем заявки по пользователю
+
+    if (serviceC.currentItem.userAccountId.isNotEmpty) {
+      filter.compare.add(name: '${TaskDocGenerated.nameAssigneeId}.${UserAccountGenerated.nameMainUserAccountId}', value: serviceC.currentItem.userAccountId);
+    }
+    if (projectController.currentItem.id != "") {
+      if (taskBoardController.currentItem.sortBy == ESorting.dateAsc) {
+        filter.compare.add(name: TaskDocGenerated.nameProjectId, value: projectController.currentItem.id);
+        filter.sorting = "${TaskDocGenerated.nameDate}+";
+      }
+      if (taskBoardController.currentItem.sortBy == ESorting.dateDesc) {
+        filter.compare.add(name: TaskDocGenerated.nameProjectId, value: projectController.currentItem.id);
+        filter.sorting = "${TaskDocGenerated.nameDate}-";
+      }
+      if (taskBoardController.currentItem.sortBy == ESorting.priorityAsc) {
+        filter.compare.add(name: TaskDocGenerated.nameProjectId, value: projectController.currentItem.id);
+        filter.sorting = "${TaskDocGenerated.namePriority}+";
+      }
+      if (taskBoardController.currentItem.sortBy == ESorting.priorityDesc) {
+        filter.compare.add(name: TaskDocGenerated.nameProjectId, value: projectController.currentItem.id);
+        filter.sorting = "${TaskDocGenerated.namePriority}-";
+      }
+      filter.compare.add(name: TaskDocGenerated.nameProjectId, value: projectController.currentItem.id);
+    }
     filter.compare.add(name: TaskDocGenerated.nameTaskStatusId, value: status);
     return await tasks.requestItems(filter: filter);
   }
