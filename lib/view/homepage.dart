c40a73f508f0de8ecc51e0249cf1d4c1343b3396
naveenpardeep <naveensheoran1000@gmail.com>
2023-04-18 10:50:36 +0300

    fix

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -319,27 +319,20 @@ class _HomepageState extends State<Homepage> {
                       Row(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          
                           Expanded(
                             child: ResizableWidget(
-                             percentages: width>750? [0.65,0.35]: [0,1],
-                              isHorizontalSeparator: false, isDisabledSmartHide: false, separatorColor: Colors.blue, separatorSize: 10, children: [
-                                               
-                          
-                              const SizedBox(),
-                              Align(
-                                
-                                alignment: Alignment.centerRight,
-                                child: taskController.obx((state) => Container(key: GlobalKey(), child: const TaskViewPage())),
-                              ),
-                              //  ResizableWidget(
-                              //         isHorizontalSeparator: true,
-                              //         isDisabledSmartHide: true,
-                              //         separatorColor: Colors.blue,
-                              //         separatorSize: 10,
-                              //         children: [ SizedBox() ]),
-                                      
-                            ]),
+                                percentages: width > 750 ? [0.65, 0.35] : [0, 1],
+                                isHorizontalSeparator: false,
+                                isDisabledSmartHide: true,
+                                separatorColor: Colors.blue,
+                                separatorSize: 10,
+                                children: [
+                                  const SizedBox(),
+                                  Align(
+                                    alignment: Alignment.centerRight,
+                                    child: taskController.obx((state) => Container(key: GlobalKey(), child: const TaskViewPage())),
+                                  ),
+                                ]),
                           ),
                         ],
                       ),
