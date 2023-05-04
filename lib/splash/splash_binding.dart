ca1d976ef694f0f983d06ced2f55bf9f2b3644a5
Mishasik <mishelzenkov@gmail.com>
2023-05-04 16:22:28 +0300

    Update splash_binding.dart

--- lib/splash/splash_binding.dart
+++ lib/splash/splash_binding.dart
@@ -4,6 +4,7 @@ import 'package:task_manager_app/forms/project/project_controller.dart';
 import 'package:task_manager_app/forms/user_account/user_account_controller.dart';
 
 import '../forms/organization/organization_controller.dart';
+import '../forms/user_account/service_object_controller.dart';
 import '../model/data_controller.dart';
 
 // ignore: deprecated_member_use
@@ -15,5 +16,6 @@ class SplashBinding extends Bindings {
     Get.put(InvitationController());
     Get.put(ProjectController());
     Get.put(OrganizationController());
+    Get.put(ServiceObjectController(), permanent: true);
   }
 }
