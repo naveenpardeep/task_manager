6dc733cda6d75960c67817984a547e174c34dc84
pro5 <pro5@SRV-FRZ-WEB>
2023-05-23 17:25:04 +0300

    Invitation filters

--- lib/forms/invitation/acceptController.dart
+++ lib/forms/invitation/acceptController.dart
@@ -2,9 +2,10 @@
 
 import 'package:get/get.dart';
 import 'package:nsg_data/nsg_data.dart';
-import 'package:task_manager_app/forms/user_account/user_account_controller.dart';
 
 import 'package:task_manager_app/model/data_controller_model.dart';
+
+import '../../model/data_controller.dart';
 
 class AccpetController extends NsgDataController<Invitation> {
   AccpetController() : super(requestOnInit: false, autoRepeate: true, autoRepeateCount: 100);
@@ -12,8 +13,10 @@ class AccpetController extends NsgDataController<Invitation> {
   @override
   NsgDataRequestParams get getRequestFilter {
     var cmp = NsgCompare();
-    var userC = Get.find<UserAccountController>();
-    cmp.add(name: InvitationGenerated.nameAuthorId, value: userC.items, comparisonOperator: NsgComparisonOperator.inList);
+    cmp.logicalOperator = NsgLogicalOperator.or;
+    var user = Get.find<DataController>().currentUser;
+    cmp.add(name: InvitationGenerated.nameAuthorId, value: user);
+    cmp.add(name: '${InvitationGenerated.nameAuthorId}.${UserAccountGenerated.nameMainUserAccountId}', value: user);
     return NsgDataRequestParams(compare: cmp);
   }
 }
--- lib/forms/invitation/invitation_controller.dart
+++ lib/forms/invitation/invitation_controller.dart
@@ -1,23 +1,24 @@
 import 'package:get/get.dart';
 import 'package:nsg_data/nsg_data.dart';
-import 'package:task_manager_app/forms/user_account/user_account_controller.dart';
 
 import 'package:task_manager_app/model/data_controller_model.dart';
 
+import '../../model/data_controller.dart';
+
 class InvitationController extends NsgDataController<Invitation> {
-  InvitationController()
-      : super(requestOnInit: false, autoRepeate: true, autoRepeateCount: 100);
+  InvitationController() : super(requestOnInit: false, autoRepeate: true, autoRepeateCount: 100);
 
   @override
   NsgDataRequestParams get getRequestFilter {
     var cmp = NsgCompare();
-    var userC = Get.find<UserAccountController>();
     cmp.add(name: InvitationGenerated.nameIsAccepted, value: false);
     cmp.add(name: InvitationGenerated.nameIsRejected, value: false);
-    cmp.add(
-        name: InvitationGenerated.nameInvitedUserId,
-        value: userC.items,
-        comparisonOperator: NsgComparisonOperator.inList);
+    var user = Get.find<DataController>().currentUser;
+    var cmpOr = NsgCompare();
+    cmpOr.logicalOperator = NsgLogicalOperator.or;
+    cmpOr.add(name: InvitationGenerated.nameInvitedUserId, value: user);
+    cmpOr.add(name: '${InvitationGenerated.nameInvitedUserId}.${UserAccountGenerated.nameMainUserAccountId}', value: user);
+    cmp.add(name: '', value: cmpOr);
     return NsgDataRequestParams(compare: cmp);
   }
 }
